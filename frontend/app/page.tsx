5e53c043981aa37e14a1c2ca7b91f4582a8b2f23
Bruno Vieira Nobre <104945877+BrunoV7@users.noreply.github.com>
2025-03-26 12:12:50 +0000

    Adicionar configuração inicial do projeto Next.js com Tailwind CSS e componentes básicos

---
+++ frontend/app/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="tracking-[-.01em]">
+            Isso aqui ainda não pronto
+          </li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="/v1/auth"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Ir para o login
+          </a>
+        </div>
+      </main>
+
+    </div>
+  );
+}
---
+++ frontend/app/v1/auth/page.tsx
@@ -0,0 +1,9 @@
+import Login from "@/components/block/v1/login"
+
+export default function LoginPage(){
+    return(
+        <div>
+            <Login></Login>
+        </div>
+    )
+}
