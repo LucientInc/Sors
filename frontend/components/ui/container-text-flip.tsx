b4d09a6196a1062f0b2dba04b36d17b05bac9d9f
Bruno Vieira Nobre <104945877+BrunoV7@users.noreply.github.com>
2025-04-06 16:29:17 +0000

    small fix

--- frontend/components/ui/container-text-flip.tsx
+++ frontend/components/ui/container-text-flip.tsx
@@ -32,8 +32,7 @@ export function ContainerTextFlip({
 
   const updateWidthForWord = () => {
     if (textRef.current) {
-      // Add some padding to the text width (30px on each side)
-      // @ts-ignore
+      // @ts-expect-error  Add some padding to the text width (30px on each side)
       const textWidth = textRef.current.scrollWidth + 0;
       setWidth(textWidth);
     }
