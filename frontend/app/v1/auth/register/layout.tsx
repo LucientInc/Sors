974457d6669f3104191096a2b5e96281006d4f4a
Bruno Vieira Nobre <104945877+BrunoV7@users.noreply.github.com>
2025-04-11 16:57:43 -0300

    feat: update metadata for layout and add new layout for registration page

---
+++ frontend/app/v1/auth/register/layout.tsx
@@ -0,0 +1,37 @@
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+export const metadata: Metadata = {
+    title: "Sors | Gerencie suas finanças com inteligência",
+    description: "Sors é um gerenciador de finanças pessoais que ajuda você a organizar, planejar e crescer financeiramente com praticidade e segurança.",
+    keywords: ["finanças pessoais", "controle financeiro", "planejamento financeiro", "gestão de dinheiro", "Sors", "app de finanças"],
+    authors: [{ name: "Sors Team" }],
+    creator: "Sors",
+    robots: {
+      index: true,
+      follow: true,
+    },
+  };
+
+  const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+    preload: true
+  })
+
+  export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <body
+          className={`${inter.className} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    );
+  }
+  
